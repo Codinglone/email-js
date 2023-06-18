c3db205fdab156dfddad8e61dd9b259e5c40617b
codinglone <codinglone@gmail.com>
2023-06-18 19:29:47 +0400

    Replaced emailjs with @emailjs/browser package

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-function App() {
+import { useRef, useState } from "react"
+import emailjs from "@emailjs/browser"
 
+
+function App() {
+  const [feedBack, setFeedBack] = useState(null)
+  const form = useRef()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_aov11xg', 'template_gwnjx9d', form.current, '67DjTDyEebmuWjNa0')
+    .then((result) => {
+      setFeedBack(result.text)
+    },
+    (error) => {
+      setFeedBack(error)
+    })
+  }
   return (
     <main>
-      <form>
-      <input type="email" id="email" required />
+      <h1>Sending E-mails with React and Email JS </h1>
+      {feedBack && <h1>{feedBack}</h1>}
+      <form ref={form}>
+      <input type="text" name="user_name" required onChange={(e) => setEmail(e.target.value)} />
+      <br />
+      <input type="email" name="user_email" required onChange={(e) => setEmail(e.target.value)} />
       <br />
       <h1>Enter a message</h1>
-      <textarea  id="message" cols="30" rows="10"></textarea>
+      <textarea  name="message" cols="30" rows="10" required onChange={(e) => setMessage(e.target.value)}></textarea>
       <br />
+      <button onClick={handleSubmit}>Send E-mail</button>
       </form>
     </main>
   )
